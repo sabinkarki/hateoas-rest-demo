db7ad4c635ec83203ab9d6441be2fb3092309974
sabin <nibaserc@gmail.com>
2017-05-03 00:15:42 -0500

    SpringBoot Set up and .gitIgnorefileAdded

---
+++ src/main/java/com/practice/demo/HateoasRestDemoApplication.java
@@ -0,0 +1,14 @@
+package com.practice.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@SpringBootApplication
+@EnableWebMvc
+public class HateoasRestDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HateoasRestDemoApplication.class, args);
+	}
+}
