1349fce78e10356997521dd901a9c30671f84364
sabin <nibaserc@gmail.com>
2017-05-03 11:33:50 -0500

    Domain class added

---
+++ src/main/java/com/practice/demo/domain/Movie.java
@@ -0,0 +1,70 @@
+package com.practice.demo.domain;
+
+public class Movie {
+
+	private long id;
+	private String name;
+
+	private String directorName;
+	private String rating;
+	private int movieInStock;
+	private Artist artist;
+
+	public Movie(long id, String name, String directorName, String rating, int movieInStock, Artist artist) {
+		this.id = id;
+		this.name = name;
+		this.directorName = directorName;
+		this.rating = rating;
+		this.movieInStock = movieInStock;
+		this.artist = artist;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDirectorName() {
+		return directorName;
+	}
+
+	public void setDirectorName(String directorName) {
+		this.directorName = directorName;
+	}
+
+	public String getRating() {
+		return rating;
+	}
+
+	public void setRating(String rating) {
+		this.rating = rating;
+	}
+
+	public int getMovieInStock() {
+		return movieInStock;
+	}
+
+	public void setMovieInStock(int movieInStock) {
+		this.movieInStock = movieInStock;
+	}
+
+	public Artist getArtist() {
+		return artist;
+	}
+
+	public void setArtist(Artist artist) {
+		this.artist = artist;
+	}
+
+}
